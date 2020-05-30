b47fd1c61876304046c8e95b7dec861e2c4d80be
shket06 <shket@live.fr>
2020-05-30 15:05:54 +0300

    это мы с Пашкой удалили всякое дверьмо и инициировали новый репозитарий

---
+++ js/mine.js
@@ -0,0 +1,54 @@
+"use strict";
+
+// const numberOffFilms = +prompt('сколько фильмов вы уже посмотрели', "");
+
+// const personalMovieDB = {
+//       count: numberOffFilms,
+//       movies: {},
+//       actors: {},
+//       genres: [],
+//       privat: false,
+// };
+
+// const a = prompt('Один из последних просмотренных фильмов', ''),
+//       b = prompt('На сколько оцените его', ''),
+//       c = prompt('Один из последних просмотренных фильмов', ''),
+//       d = prompt('На сколько оцените его', '');
+
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+// console.log(personalMovieDB);
+
+if (4 == 9) {
+      console.log('ok');
+
+} else {
+    console.log('error');
+}
+
+// const num = 50;
+
+// if (num < 49) {
+//       console.log('error');
+// } else if (num > 100) {
+//       console.log('beaucoup');
+// }else {
+//       console.log('ok');
+// }
+
+// (num == 50) ? console.log('ok') : console.log('non');
+
+const num = 50;
+
+switch (num) {
+      case 49:
+            console.log('non');
+            break;
+      case 100:
+            console.log('non');
+            break;
+      case 50:
+            console.log('yes');
+            break;
+}
