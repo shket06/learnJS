6c9bd93377b031d4840946f9beeff62faffcc7bc
shket06 <shket@live.fr>
2020-06-01 16:23:32 +0300

    циклы

--- js/mine.js
+++ js/mine.js
@@ -1,54 +1,24 @@
 "use strict";
 
-// const numberOffFilms = +prompt('сколько фильмов вы уже посмотрели', "");
+let num = 50;
 
-// const personalMovieDB = {
-//       count: numberOffFilms,
-//       movies: {},
-//       actors: {},
-//       genres: [],
-//       privat: false,
-// };
-
-// const a = prompt('Один из последних просмотренных фильмов', ''),
-//       b = prompt('На сколько оцените его', ''),
-//       c = prompt('Один из последних просмотренных фильмов', ''),
-//       d = prompt('На сколько оцените его', '');
-
-// personalMovieDB.movies[a] = b;
-// personalMovieDB.movies[c] = d;
-
-// console.log(personalMovieDB);
-
-if (4 == 9) {
-      console.log('ok');
-
-} else {
-    console.log('error');
-}
-
-// const num = 50;
-
-// if (num < 49) {
-//       console.log('error');
-// } else if (num > 100) {
-//       console.log('beaucoup');
-// }else {
-//       console.log('ok');
+// while (num < 55) {
+//       console.log(num);
+//       num++;
 // }
 
-// (num == 50) ? console.log('ok') : console.log('non');
+// do {
+//       console.log(num);
+//       num++;
+// }
+// while (num < 55);
 
-const num = 50;
+for ( let i = 1; i < 8; i++) {
+      
 
-switch (num) {
-      case 49:
-            console.log('non');
-            break;
-      case 100:
-            console.log('non');
-            break;
-      case 50:
-            console.log('yes');
-            break;
+
+
+
+      console.log(i);
+    
 }
